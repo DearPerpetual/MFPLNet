312d848350caef3b3240f8168c877d591771c38a
DearPerpetual <65269483+DearPerpetual@users.noreply.github.com>
2024-09-25 17:10:41 +0800

    Add files via upload

---
+++ configs/clrnet/clr_swin_t_tusimple.py
@@ -0,0 +1,106 @@
+net = dict(type='Detector', )
+
+num_points = 72
+max_lanes = 10
+sample_y = range(540, -1, -1)
+
+heads = dict(type='CLRHead',
+             num_priors=192,
+             refine_layers=3,
+             fc_hidden_dim=64,
+             sample_points=36)
+
+iou_loss_weight = 2
+cls_loss_weight = 6.
+xyt_loss_weight = 0.5
+seg_loss_weight = 1.0
+
+work_dirs = "work_dirs/out/swin_t_tusimple"
+
+neck = dict(type='FPN',
+            in_channels=[192, 384, 768],
+            out_channels=64,
+            num_outs=3,
+            attention=False)
+
+test_parameters = dict(conf_threshold=0.40, nms_thres=50, nms_topk=max_lanes)
+epochs = 400
+batch_size = 8
+optimizer = dict(type='AdamW', lr=1.0e-3)
+total_iter = (3616 // batch_size + 1) * epochs
+scheduler = dict(type='CosineAnnealingLR', T_max=total_iter)
+eval_ep = 1
+save_ep = 5
+
+img_norm = dict(mean=[103.939, 116.779, 123.68], std=[1., 1., 1.])
+ori_img_w = 360
+ori_img_h = 540
+img_h = 540
+img_w = 360
+cut_height = 0
+
+train_process = [
+    dict(
+        type='GenerateLaneLine',
+        transforms=[
+            dict(name='Resize',
+                 parameters=dict(size=dict(height=img_h, width=img_w)),
+                 p=1.0),
+            dict(name='HorizontalFlip', parameters=dict(p=1.0), p=0.5),
+            dict(name='ChannelShuffle', parameters=dict(p=1.0), p=0.1),
+            dict(name='MultiplyAndAddToBrightness',
+                 parameters=dict(mul=(0.85, 1.15), add=(-10, 10)),
+                 p=0.6),
+            dict(name='AddToHueAndSaturation',
+                 parameters=dict(value=(-10, 10)),
+                 p=0.7),
+            dict(name='OneOf',
+                 transforms=[
+                     dict(name='MotionBlur', parameters=dict(k=(3, 5))),
+                     dict(name='MedianBlur', parameters=dict(k=(3, 5)))
+                 ],
+                 p=0.2),
+            dict(name='Affine',
+                 parameters=dict(translate_percent=dict(x=(-0.1, 0.1),
+                                                        y=(-0.1, 0.1)),
+                                 rotate=(-10, 10),
+                                 scale=(0.8, 1.2)),
+                 p=0.7),
+            dict(name='Resize',
+                 parameters=dict(size=dict(height=img_h, width=img_w)),
+                 p=1.0),
+        ],
+    ),
+    dict(type='ToTensor', keys=['img', 'lane_line', 'seg']),
+]
+
+val_process = [
+    dict(type='GenerateLaneLine',
+         transforms=[
+             dict(name='Resize',
+                  parameters=dict(size=dict(height=img_h, width=img_w)),
+                  p=1.0),
+         ],
+         training=False),
+    dict(type='ToTensor', keys=['img']),
+    #dict(type='ToTensor', keys=['img','seg']),
+]
+
+dataset_path = './data/tusimple'
+dataset_type = 'TuSimple'
+test_json_file = 'data/tusimple/test_label.json'
+dataset = dict(
+val=dict(
+    type=dataset_type,
+    data_root=dataset_path,
+    split='test',
+    processes=val_process,
+))
+
+workers = 10
+log_interval = 100
+# seed = 0
+num_classes = 6 + 1
+ignore_label = 255
+bg_weight = 0.4
+lr_update_by_epoch = False
